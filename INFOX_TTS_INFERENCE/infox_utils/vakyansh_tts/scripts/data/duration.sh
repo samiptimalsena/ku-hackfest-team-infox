c6601075093b2bb321bb1369cc6d3d03e07443e9
samiptimalsena <samip425@gmail.com>
2022-08-05 22:20:43 +0545

    refactored the directory

--- INFOX_TTS_INFERENCE/infox_utils/vakyansh_tts/scripts/data/duration.sh
+++
@@ -1,9 +0,0 @@
-wav_path='/home/harveen/en/iitm_data/english/wav_22k'
-#######################
-
-dir=$PWD
-parentdir="$(dirname "$dir")"
-parentdir="$(dirname "$parentdir")"
-
-
-python $parentdir/utils/data/duration.py $wav_path
--- INFOX_TTS_INFERENCE/infox_utils/vakyansh_tts/scripts/inference/gradio.sh
+++
@@ -1,8 +0,0 @@
-gender='male'
-glowdir='../../checkpoints/glow/'$gender'/'
-hifidir='../../checkpoints/hifi/'$gender'/'
-device='cpu'
-lang='en'
-
-
-python ../../utils/inference/run_gradio.py -a $glowdir -v $hifidir -d $device -L $lang
